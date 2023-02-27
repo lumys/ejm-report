e9e06037a3dfe7696f8f977a48cf5dd971ccd18b
kim young hwan <yhkim@cuuva.com>
2023-02-27 17:17:41 +0900

    MUI 라이브러리 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { ThemeProvider } from '@mui/material';
 import { RouterProvider } from 'react-router-dom';
 import router from '~/routes/rootRouter';
-import '~/App.css';
+import theme from '@styles/theme';
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '~/index.css';
+import { CssBaseline } from '@mui/material';
 import App from '~/App';
 import reportWebVitals from '~/reportWebVitals';
 
@@ -10,6 +10,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
 );
