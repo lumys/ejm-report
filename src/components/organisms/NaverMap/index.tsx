4d80f1e7c19d82d695ce526c3da49913cda60f81
kim young hwan <yhkim@cuuva.com>
2023-02-27 22:45:56 +0900

    NaverMap 컴포넌트 Props 변경

--- src/components/organisms/NaverMap/index.tsx
+++ src/components/organisms/NaverMap/index.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import {
-  NaverMap,
-  Container as MapDiv,
-  useNavermaps,
-  Marker,
-} from 'react-naver-maps';
+import { NaverMap, Container as MapDiv, NaverMapProps } from 'react-naver-maps';
 
-const Component = () => {
-  const navermaps = useNavermaps();
-
+const Component = (props: NaverMapProps) => {
   return (
     <MapDiv style={{ width: '100%', height: '100%' }}>
-      <NaverMap defaultZoom={8}>
-        <Marker position={new navermaps.LatLng(37.3595704, 127.105399)} />
-      </NaverMap>
+      <NaverMap {...props} />
     </MapDiv>
   );
 };
--- src/pages/LocationPage/Template/index.tsx
+++ src/pages/LocationPage/Template/index.tsx
@@ -2,14 +2,10 @@ import React from 'react';
 import { Box } from '@mui/material';
 import NaverMap from '@components/organisms/NaverMap';
 
-const mapOptions = {
-  zoom: 8,
-};
-
 const Template = () => {
   return (
     <Box sx={{ flex: 1 }}>
-      <NaverMap />
+      <NaverMap zoom={8} />
     </Box>
   );
 };
